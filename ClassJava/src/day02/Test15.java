2072e6b38746d2ef83afa627278f4a3a634aa096
user <user@user-PC>
2019-10-11 16:56:08 +0900

    10/11 edit

--- ClassJava/src/day02/Test15.java
+++ ClassJava/src/day02/Test15.java
@@ -27,11 +27,11 @@ package day02;
 	
 	------------------------
 4.
-	 1
-	 2  3
-	 4  5  6
-	 7  8  9 10
-	11 12 13 14 15
+	  1  2  3  4  5
+	  6  7  8  9
+	 10 11 12
+	 13 14
+	 15
 	
 	------------------------
 5.
@@ -46,7 +46,14 @@ package day02;
 public class Test15 {
 
 	public Test15() {
-		
+		int num = 0;
+		for(int i = 0; i < 5; i++ ) {
+			for(int j = 0 ; j < 5 ; j++ ) {
+				num += 1;
+				System.out.printf("%2d ", num);
+			}
+			System.out.println();
+		}
 	}
 	
 	public static void main(String[] args) {
