cbe61bee2267b197f4b5c351e08d0529b45b5b9e
user <user@user-PC>
2019-10-14 18:05:17 +0900

    day03 last commit

---
+++ ClassJava/src/day03/Ex05.java
@@ -0,0 +1,14 @@
+package day03;
+
+public class Ex05 {
+
+	public Ex05() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
