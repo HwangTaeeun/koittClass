cc3ec6d95c6ab0600ab9ff2eb67b356c46e1b517
user <user@user-PC>
2019-10-18 15:29:28 +0900

    문제 커밋...

---
+++ ClassJava/src/day07/Ex04.java
@@ -0,0 +1,22 @@
+package day07;
+
+/*
+	사각형과 삼각형 클래스를 만들고
+	
+	사각형과 삼각형 을 관리할 배열을 각각 만들어서 
+	넓이를 출력하세요.
+	
+	*** 생성자함수 사용하는 방법 연습하는 문제
+ */
+
+public class Ex04 {
+
+	public Ex04() {
+		
+	}
+
+	public static void main(String[] args) {
+		new Ex04();
+	}
+
+}
