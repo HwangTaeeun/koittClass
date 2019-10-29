024f31ea292c841ea5900a939de46623322221e3
user <user@user-PC>
2019-10-29 17:55:38 +0900

    day14 last commit

---
+++ ClassJava/src/day14/Ex04.java
@@ -0,0 +1,25 @@
+package day14;
+
+import java.util.*;
+
+/*
+	Circle 클래스를 반지름과 둘레, 넓이를 저장하도록 만들고 
+	이 클래스의 인스턴스를 5개를 저장할 ArrayList를 만들어서 
+	저장하고
+	각 원들의 반지름 순으로 정렬되게 하세요.
+		방법]
+			1. Circle 을 정렬할 수 있는 상태(Comparable 을 구현한 클래스)로 만드는 경우
+			2. 정렬자(Comparator)를 만들어서 정렬하는 방법
+	
+ */
+public class Ex04 {
+
+	public Ex04() {
+		
+	}
+
+	public static void main(String[] args) {
+		new Ex04();
+	}
+
+}
---
+++ ClassJava/src/day14/Ex05.java
@@ -0,0 +1,24 @@
+package day14;
+
+import java.util.*;
+
+/*
+	문제 5 ]
+		삼각형 클래스를 만들고
+		ArrayList에 7개의 삼각형을 만들어서 넣은 후
+			1. 높이가 작은 순부터 정렬하고
+			2. 밑변이 넓은 순으로 정렬하고
+			3. 넓이가 큰 순으로 정렬하세요.
+ */
+
+public class Ex05 {
+
+	public Ex05() {
+		
+	}
+
+	public static void main(String[] args) {
+		new Ex05();
+	}
+
+}
